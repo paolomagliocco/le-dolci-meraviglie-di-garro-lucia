c02ef08cda2d5a372bcc0bd0a88ece6b9f81fa74
paolomagliocco <paolomagliocco89@gmail.com>
2026-08-28 18:13:34 +0200

    aggiunta del sitemap e del robots nel progetto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.esempio-biscotti.it',
+  site: 'https://www.ledolcimeravigliedigarrolucia.com',
   integrations: [sitemap()],
 });
